a6bb1c39b873d78a7334aafd5d6ca12c7fc86b2c
nicola <rusco@nicolasanti.it>
2016-06-13 16:03:02 +0200

    dns

--- bin/services/rabbit-console.sh
+++ bin/services/rabbit-console.sh
@@ -1,4 +1,4 @@
-docker run -it --rm --link rabbitmq:ami-01 --add-host ami-01:172.17.8.101  --add-host ami-02:172.17.8.102  -e RABBITMQ_ERLANG_COOKIE='tokensupersegreto' -e RABBITMQ_NODENAME='rabbit@ami-01' rabbitmq:3 bash
+docker run -it --rm --link rabbitmq:ami-01.anarchia.loc    -e RABBITMQ_ERLANG_COOKIE='tokensupersegreto' -e RABBITMQ_NODENAME='rabbit@ami-01.anarchia.loc' rabbitmq:3 bash
 
 
 #docker run -d --hostname ami-01 --name some-rabbit -p 8080:15672 rabbitmq:3-management
